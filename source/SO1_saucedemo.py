02f8962ad76be727f7f2254b64367c47e04c893d
Direction <direction@nstgroupe.com>
2023-03-05 13:14:40 +0100

    first example selenium

--- source/SO1_saucedemo.py
+++ source/SO1_saucedemo.py
@@ -11,3 +11,13 @@ driver = webdriver.Chrome(service=service)
 
 # Ouvrir le site saucedemo.com
 driver.get("https://www.saucedemo.com/")
+username= driver.find_element(By.ID,"user-name")
+password_input = driver.find_element(By.ID,"password")
+password_input.send_keys("secret_sauce")
+username.send_keys("standard_user")
+login_button = driver.find_element(By.ID,"login-button")
+login_button.click()
+time.sleep(5)
+assert "Login" not in driver.page_source
+
+driver.quit()
