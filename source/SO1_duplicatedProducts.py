7e0cc3552a701c950a1bce874bfd7fdfb71eeb39
omar siala <siala.omar@gmail.com>
2023-03-07 17:26:09 +0100

    fix issue with right product name element

--- source/SO1_duplicatedProducts.py
+++ source/SO1_duplicatedProducts.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
 # Chemin du driver de navigateur
-driver_path = r"D:\Formations_omar\ChromeDriver\chromedriver.exe"
+driver_path = r"C:\Formations_omar\tools\chromedriver_win32\chromedriver.exe"
 service = Service(executable_path=driver_path)
 
 # Instanciation du driver
@@ -21,11 +21,11 @@ time.sleep(5)
 assert "Login" not in driver.page_source
 
 product_names =[]
-products_images = driver.find_elements(By.CLASS_NAME, "inventory_item_img")
-for product_image in products_images:
-    if(product_image.text != None):
-        product_names.append(product_image.text)
-print(product_names)
+products_namesElm = driver.find_elements(By.CLASS_NAME, "inventory_item_name")
+for product_name in products_namesElm:
+    if(product_name.text != None):
+        product_names.append(product_name.text)
+# print(product_names)
 
 # Vérifie si la liste des images de produits n'est pas dupliquée
 assert len(product_names) == len(set(product_names)), "La liste des images de produits est dupliquée."
